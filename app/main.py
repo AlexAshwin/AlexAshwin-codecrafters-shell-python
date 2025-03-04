9d00b7d43cd103325b5c8b1aba282ee6ed8511d5
AlexAshwin <alxgrg123@gmail.com>
2025-03-04 18:29:38 +0530

    rewrote type

--- app/main.py
+++ app/main.py
@@ -7,24 +7,16 @@ def main():
 
         # Wait for user input
         command = input()
-        builtin = ['type','echo','exit']
         if command == "exit" or command == "exit 0":
             break
         elif command.startswith("echo"):
             print(command[5:])
         elif command.startswith("type"):
-            if command[5:] in builtin:
-                if  path := shutil.which(command[5:]):
-                    print(f"{command[5:]} is a shell builtin")
-                    print(f"{command[5:]} is {path}")
-                else:
-                    print(f"{command[5:]}: command not found")
-                
-            elif command[5:] == "":
-                print("Please provide a command to check") 
+            if  path := shutil.which(command[5:]):
+                 print(f"{command[5:]} is {path}")
             else:
-                print(f"{command[5:]}: not found")
-        else:
-            print(f"{command}: command not found")
+                print(f"{command[5:]}: command not found")
+        elif command[5:] == "":
+            print("Please provide a command to check")
 if __name__ == "__main__":
     main()
