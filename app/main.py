62b8d22993d507b80d7a164e6de9df43d044240a
AlexAshwin <alxgrg123@gmail.com>
2025-03-04 21:59:47 +0530

    added execution of process

--- app/main.py
+++ app/main.py
@@ -1,30 +1,43 @@
 import shutil
 import sys
+import os
+
+def handler_echo(args):
+    print(args)
+
+def handler_exit():
+    exit(0)
+
+def handler_type(args,builtin):
+    if args in builtin:
+        print(f"{args} is a shell builtin")
+    else:
+        path = shutil.which(args)
+        if path:
+            print(f"{args} is {path}")
+        else:
+            print(f"{args}: not found")
+
+def check_executable(args=None):
+
+    script_path = shutil.which(args.split()[0])
+    if script_path:
+        os.system(args)
+    else:
+        print(f"{args}: not found")
+
+builtin = {"echo": handler_echo, "exit": handler_exit, "type": handler_type}
 
 def main():
     while True:
         # Uncomment this block to pass the first stage
         sys.stdout.write("$ ")
-        builtin = ["exit", "echo", "type"]
         # Wait for user input
         command = input()
-        if command == "exit" or command == "exit 0":
-            break
-        elif command.startswith("echo"):
-            print(command[5:])
-        elif command.startswith("type"):
-            if command[5:] in builtin:
-                print(f"{command[5:]} is a shell builtin")
-            else:
-                path = shutil.which(command[5:])
-                if path:
-                    print(f"{command[5:]} is {path}")
-                else:
-                    print(f"{command[5:]}: not found")
-        elif not command.strip():
-            print("Please provide a command to check")
+        if command.split()[0] in builtin:
+            builtin[command.split()[0]](command.split()[1:],builtin)
         else:
-            print(f"{command}: command not found")
+            check_executable(command)
 
 if __name__ == "__main__":
     main()
