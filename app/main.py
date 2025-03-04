f4876df8e51ae6b5a6d91abc5946ee20223bef32
AlexAshwin <alxgrg123@gmail.com>
2025-03-04 16:51:52 +0530

    modified echo

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 import sys
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    if command == "exit 0":
-        sys.exit(0)
-    elif command[0:4] == "echo":
-        print(command[5:])
-        main()
-    else:
-        print(f"{command}: command not found")
-        main()
+        # Wait for user input
+        command = input()
+        command, *args = command.split(" ")
+        if command == "exit 0":
+            break
+        elif command[0:4] == "echo":
+            print(" ".join(args))
+            main()
+        else:
+            print(f"{command}: command not found")
+            main()
 if __name__ == "__main__":
     main()
