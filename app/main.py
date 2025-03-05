74ce38c648b6249921796c42e17c7e82b88910f2
AlexAshwin <alxgrg123@gmail.com>
2025-03-05 18:19:35 +0530

    testing chnages

--- app/main.py
+++ app/main.py
@@ -1,96 +1,70 @@
 import sys
-import os
-import subprocess
-
-def handler_echo(args=None):
-    # Check for output redirection
-    if "1>" in args:
-        args = args.split(" 1> ")  # Remove space after '1>'
-        args[0] = args[0].strip("'")  # Clean up the string
-        with open(args[1], 'w') as f:
-            f.write(args[0])
-            f.write("\n")
-    elif ">" in args:
-        args = args.split("> ")  # Remove space after '>'
-        args[0] = args[0].strip("'")  # Clean up the string
-        with open(args[1], 'w') as f:
-            f.write(args[0])
-            f.write("\n")
-    else:
-        print(args)  # Print to terminal if no redirection
-
-def handler_exit(args=None):
-    exit(0)
-
-def handler_type(args):
-    if args in builtin:
-        print(f"{args} is a shell builtin")
-    else:
-        path = find_executable(args)
-        if path:
-            print(f"{args} is {path}")
-        else:
-            print(f"{args}: not found")
-
-def find_executable(command):
-    for dir in os.environ.get("PATH", "").split(os.pathsep):
-        executable = os.path.join(dir, command)
-        if os.path.isfile(executable) and os.access(executable, os.X_OK):
-            return executable
-    return None
-
-def check_executable(args):
-    script_path = find_executable(args.split()[0])
-    if script_path:
-        # Check if the command contains the '>' operator
-        if '>' in args:
-            command, file_name = args.split('>', 1)
-            file_name = file_name.strip()
-            with open(file_name, 'w') as f:
-                subprocess.run(command, shell=True, stdout=f)
-            return
-        subprocess.run(args, shell=True)
-    else:
-        print(f"{args.split()[0]}: not found")  # Only show the command part, not the redirection part
-
-def handler_pwd(args=None):
-    print(os.getcwd())
-    sys.stdout.write("$ ")
-
-def handler_change_directory(args):
-    if args == '~':
-        os.chdir(os.path.expanduser('~'))
-    elif args == '/':
-        os.chdir('/')
-    else:
-        try:
-            os.chdir(args)
-        except FileNotFoundError:
-            print(f"cd: {args}: No such file or directory")
-    sys.stdout.write("$ ")
-
-
-builtin = {"echo": handler_echo, "exit": handler_exit, "type": handler_type, "pwd": handler_pwd, "cd": handler_change_directory}
-
+from os import chdir, environ, getcwd, path
+import shlex
+from contextlib import redirect_stdout, redirect_stderr
+from subprocess import run, PIPE
+all_builtin_cmd = ["exit", "echo", "type", "pwd", "cd"]
 def main():
     sys.stdout.write("$ ")
-    while True:
-        command = input().strip()
-
-        if not command:
-            continue
-
-        parts = command.split(maxsplit=1)
-        if len(parts) > 1:
-            cmd, args = parts
-        else:
-            cmd, args = parts[0], None
-
-        if cmd in builtin:
-            builtin[cmd](args)
-        else:
-            check_executable(command)
-        sys.stdout.write("$ ")
-
+    # Wait for user input
+    cmd = input()
+    match shlex.split(cmd):
+        case [*run_cmd, ">>", file] | [*run_cmd, "1>>", file]:
+            with open(file, "a") as out_file:
+                with redirect_stdout(out_file):
+                    handle_cmd(" ".join(run_cmd))
+        case [*run_cmd, ">", file] | [*run_cmd, "1>", file]:
+            with open(file, "w") as out_file:
+                with redirect_stdout(out_file):
+                    handle_cmd(" ".join(run_cmd))
+        case [*run_cmd, "2>>", file]:
+            with open(file, "a") as err_file:
+                with redirect_stderr(err_file):
+                    handle_cmd(" ".join(run_cmd))
+        case [*run_cmd, "2>", file]:
+            with open(file, "w") as err_file:
+                with redirect_stderr(err_file):
+                    handle_cmd(" ".join(run_cmd))
+        case _:
+            handle_cmd(cmd)
+    main()
+def handle_cmd(cmd):
+    match shlex.split(cmd):
+        case ["exit", "0"]:
+            sys.exit(0)
+        case ["echo", *args]:
+            print(*args)
+        case ["type", arg]:
+            print(type_cmd(arg)[1])
+        case ["pwd"]:
+            print(getcwd())
+        case ["cd", arg]:
+            cd_cmd(arg)
+        case [found_cmd, *args] if type_cmd(found_cmd)[0]:
+            process = run(shlex.split(cmd), stdout=PIPE, stderr=PIPE, text=True)
+            print(process.stdout, end="")
+            if process.stderr:
+                print(process.stderr, file=sys.stderr, end="")
+        case [found_cmd] if type_cmd(found_cmd)[0]:
+            process = run(shlex.split(cmd), stdout=PIPE, stderr=PIPE, text=True)
+            print(process.stdout, end="")
+            if process.stderr:
+                print(process.stderr, file=sys.stderr, end="")
+        case _:
+            print(f"{cmd}: command not found", file=sys.stderr)
+def type_cmd(cmd):
+    if cmd in all_builtin_cmd:
+        return False, f"{cmd} is a shell builtin"
+    PATH_ENV = environ["PATH"].split(":")
+    for path_dir in PATH_ENV:
+        file_path = path.join(path_dir, cmd)
+        if path.isfile(file_path):
+            return True, f"{cmd} is {file_path}"
+    return False, f"{cmd}: not found"
+def cd_cmd(arg):
+    try:
+        chdir(path.expanduser(arg))
+    except OSError:
+        print(f"cd: {arg}: No such file or directory", file=sys.stderr)
 if __name__ == "__main__":
     main()
