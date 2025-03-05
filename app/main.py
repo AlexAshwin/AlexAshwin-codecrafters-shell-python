d4258a49505697c8bc71f7a1d0933356a3d6fcc8
AlexAshwin <alxgrg123@gmail.com>
2025-03-05 16:33:20 +0530

    testing chnages

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ import subprocess
 
 def handler_echo(args=None):
     if ("1>" in args):
-        args = args.split("1>")
+        args = args.split(" 1> ")
         args[0] = args[0].strip("'")
         with open(args[1], 'w') as f:
             f.write(args[0])
 
     elif (">" in args):
-        args = args.split(">")
+        args = args.split(" > ")
         args[0] = args[0].strip("'")
         with open(args[1], 'w') as f:
             f.write(args[0])
